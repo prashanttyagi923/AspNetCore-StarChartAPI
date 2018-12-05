c81c7456f5e892489159a08598be3c0a9b1d8692
Prashant Tyagi <prashant.tyagi@nagarro.com>
2018-12-05 15:50:10 +0530

    Added action method in controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,6 +16,44 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-        
-    }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);// ?? NotFound();
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+            celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(x => x.Name == name).ToList();// ?? NotFound();
+
+            if (!celestialObjects.Any())
+            {
+                return NotFound();
+            }
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(celestialObjects);
+        }
+        [HttpGet)]
+        public IActionResult GetAll(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+            return Ok(celestialObjects);
+        }
+
+        }
 }
